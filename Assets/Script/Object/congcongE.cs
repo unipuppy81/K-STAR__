d1f2c27b83d6b9a8ba334ca691bbfb175696b11b
hayum0525 <khyoung9128@gmail.com>
2023-09-09 18:38:02 +0900

    congcongE3

--- Assets/Script/Object/congcongE.cs
+++ Assets/Script/Object/congcongE.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class congcongE : MonoBehaviour
 {
-    [SerializeField]
-    private float jumpForce = 400f;
-    [SerializeField]
-    private float speed = 1f;
-    [SerializeField]
-    private float jumpZoneForce = 2f;
 
-    bool isJumpZone = false;
-    Rigidbody rb;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.name=="congcongE")
-        {
-            isJumpZone = true;
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
 
-    }
+    PlayetrCTL p
+    //[SerializeField]
+    //private float jumpForce = 400f;
+    //[SerializeField]
+    //private float speed = 1f;
+    //[SerializeField]
+    //private float jumpZoneForce = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-       if(isJumpZone)
-        {
-            rb.AddForce(new Vector3(0, jumpZoneForce, 0)*jumpForce);
-            isJumpZone = false;
-        }
-       
-    }
+    //bool isJumpZone = false;
+    //Rigidbody rb;
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.name == "congcongE")
+    //    {
+    //        isJumpZone = true;
+    //    }
+    //}
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (isJumpZone)
+    //    {
+    //        rb.AddForce(new Vector3(0, jumpZoneForce, 0) * jumpForce);
+    //        // isJumpZone = false;
+    //    }
+
+    //}
 }
